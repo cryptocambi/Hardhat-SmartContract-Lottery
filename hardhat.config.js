f084f27f9b64894c380e00cd464bd83e57da27e5
Cryptocambi <cryptocambi@gmail.com>
2023-05-16 13:25:32 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL ||
+    "https://eth-sepolia.g.alchemy.com/v2/-eThWc9tsabKX_oPoSJg97lC2vaDYpRT"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfermation: 1,
+        },
+        sepolia: {
+            chainId: 11155111,
+            blockConfermation: 6,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.7",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,24 @@
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { assert } = require("chai")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle unit test", async function () {
+          let raffle, vrfCoordinatorV2Mock
+          const chainId = network.config.chainId
+          beforeEach(async function () {
+              const { deployer } = await getNamedAccounts()
+              await deployments.fixture(["all"])
+              raffle = await ethers.getContract("Raffle", deployer)
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+          })
+          describe("constructor", async function () {
+              it("initialised the raffle correctly", async function () {
+                  const raffleState = await raffle.getRaffleState()
+                  const interval = await raffle.getInterval()
+                  assert.equal(raffleState.toString(), "0")
+                  assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+              })
+          })
+      })
