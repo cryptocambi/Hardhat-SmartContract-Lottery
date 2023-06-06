c5c4c19068794ed5aceb9bdf6132a69048c0aeac
Cryptocambi <cryptocambi@gmail.com>
2023-06-06 11:22:49 +0200

    Final draft

--- test/staging/Raffle.staging.js
+++ test/staging/Raffle.staging.js
@@ -17,38 +17,38 @@ developmentChains.includes(network.name)
               it("works with live chainlink keepers and Chainlink VRF, we get a random winner", async () => {
                   const startingTimeStamp = await raffle.getLastTimeStamp()
                   const accounts = await ethers.getSigners()
-                  await new Promise(async (resolve, reject) => {
-                      raffle.once("WinnerPicked", async () => {
-                          console.log("Winner event as been fired!")
-                      })
 
-                      try {
-                          const recentWinner = await raffle.getRecentWinner()
-                          const raffleState = await raffle.getRaffleState()
-                          const winnerEndingBalance = await accounts[0].getBalance()
-                          const endingTimeStamp = await raffle.getLastTimeStamp()
-
-                          await expect(raffle.getPlayer(0)).to.be.reverted
-                          assert.equal(recentWinner.toString(), accounts[0].address)
-                          assert.equal(raffleState, 0)
-                          assert.equal(
-                              winnerEndingBalance.toString(),
-                              winnerStartingBalance.add(raffleEntranceFee).toSting()
-                          )
-                          assert(endingTimeStamp > startingTimeStamp)
-                          resolve()
-                      } catch (error) {
-                          reject(error)
-                          console.log(error)
-                      }
-                  })
-                  //await raffle.enterRaffle({ value: raffleEntranceFee })
-                  //const winnerStartingBalance = await accounts[0].getBalance()
                   console.log("Entering Raffle...")
-                  const tx = await raffle.enterRaffle({ value: raffleEntranceFee }) // changes on this line.
-                  txReceipt = await tx.wait(1) // changes on this line.
+                  const tx = await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const txReceipt = await tx.wait(1)
                   console.log("Ok, time to wait...")
                   const winnerStartingBalance = await accounts[0].getBalance()
+
+                  await new Promise(async (resolve, reject) => {
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("Winner event has been fired!")
+
+                          try {
+                              const recentWinner = await raffle.getRecentWinner()
+                              const raffleState = await raffle.getRaffleState()
+                              const winnerEndingBalance = await accounts[0].getBalance()
+                              const endingTimeStamp = await raffle.getLastTimeStamp()
+
+                              await expect(raffle.getPlayer(0)).to.be.reverted
+                              assert.equal(recentWinner.toString(), accounts[0].address)
+                              assert.equal(raffleState, 0)
+                              assert.equal(
+                                  winnerEndingBalance.toString(),
+                                  winnerStartingBalance.add(raffleEntranceFee).toString()
+                              )
+                              assert(endingTimeStamp > startingTimeStamp)
+                              resolve()
+                          } catch (error) {
+                              reject(error)
+                              console.log(error)
+                          }
+                      })
+                  })
               })
           })
       })
